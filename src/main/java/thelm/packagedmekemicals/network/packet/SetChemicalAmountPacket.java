d64a6c50f63eccc7f50010887f74a26e54da37c4
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-07-27 03:35:07 +0800

    Disallow activating chemical package filler when working
    Disallow changing amount setting of chemical package filler when working
    Change packet implementation

--- src/main/java/thelm/packagedmekemicals/network/packet/SetChemicalAmountPacket.java
+++ src/main/java/thelm/packagedmekemicals/network/packet/SetChemicalAmountPacket.java
@@ -7,27 +7,21 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
 import thelm.packagedmekemicals.menu.ChemicalPackageFillerMenu;
 
-public class SetChemicalAmountPacket {
+public record SetChemicalAmountPacket(int amount) {
 
-	private int amount;
-
-	public SetChemicalAmountPacket(int amount) {
-		this.amount = amount;
-	}
-
-	public static void encode(SetChemicalAmountPacket pkt, FriendlyByteBuf buf) {
-		buf.writeInt(pkt.amount);
+	public void encode(FriendlyByteBuf buf) {
+		buf.writeInt(amount);
 	}
 
 	public static SetChemicalAmountPacket decode(FriendlyByteBuf buf) {
 		return new SetChemicalAmountPacket(buf.readInt());
 	}
 
-	public static void handle(SetChemicalAmountPacket pkt, Supplier<NetworkEvent.Context> ctx) {
+	public void handle(Supplier<NetworkEvent.Context> ctx) {
 		ServerPlayer player = ctx.get().getSender();
 		ctx.get().enqueueWork(()->{
 			if(player.containerMenu instanceof ChemicalPackageFillerMenu menu) {
-				menu.blockEntity.requiredAmount = pkt.amount;
+				menu.blockEntity.requiredAmount = amount;
 			}
 		});
 		ctx.get().setPacketHandled(true);
