b2ad842c9e1a5d96b9f937f2b90acfd0e89ce094
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-07-04 14:17:28 +0800

    Adjust block strength values

--- src/main/java/thelm/packagedmekemicals/block/ChemicalPackageFillerBlock.java
+++ src/main/java/thelm/packagedmekemicals/block/ChemicalPackageFillerBlock.java
@@ -23,7 +23,7 @@ public class ChemicalPackageFillerBlock extends BaseBlock {
 	public static final Item ITEM_INSTANCE = new BlockItem(INSTANCE, new Item.Properties().tab(PackagedMekemicals.CREATIVE_TAB)).setRegistryName("packagedmekemicals:chemical_package_filler");
 
 	protected ChemicalPackageFillerBlock() {
-		super(BlockBehaviour.Properties.of(Material.METAL).strength(15F, 25F).sound(SoundType.METAL));
+		super(BlockBehaviour.Properties.of(Material.METAL).strength(15F, 15F).sound(SoundType.METAL));
 		setRegistryName("packagedmekemicals:chemical_package_filler");
 	}
 
